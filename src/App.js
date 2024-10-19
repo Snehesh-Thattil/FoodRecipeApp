5e05c8d3ed30667b6e7b79234884450c2829e49f
Snehesh-Thattil <sneheshthattil75@gmail.com>
2024-10-19 22:18:34 +0530

    [ Fetched Data with axios through API call : 2 ]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Menu from './Components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <h1>Food Recipe App</h1>
+      <Menu />
     </div>
-  );
+  )
 }
 
 export default App;
