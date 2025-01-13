eee983f209ea44ef8b180b83e434553c507fac12
Snehesh Thattil <sneheshthattil75@gmail.com>
2025-01-13 17:07:10 +0530

    [ Fetched more data than before with additional axios calls and prevState : 44 ]

--- src/Contexts/MealsDataContext.js
+++ src/Contexts/MealsDataContext.js
@@ -9,19 +9,22 @@ export function MealDataContextWrapper({ children }) {
     const [mealsData, setMealsData] = useState()
     const [specialDishesLoaded, setSpecialDishesLoaded] = useState(false)
 
-    // Fetching Special Dishes with API
+    // Fetching Dishes with API
     useEffect(() => {
-        function getMenu() {
-            axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=s')
-                .then((res) => {
-                    setMealsData(res.data.meals)
-                    setSpecialDishesLoaded(true)
-                })
-                .catch((err) => {
-                    console.log(err.message)
-                })
-        }
-        getMenu()
+        axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=s')
+            .then((res) => {
+                setMealsData(res.data.meals)
+                setSpecialDishesLoaded(true)
+            })
+            .then(() => {
+                axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=c')
+                    .then((res) => {
+                        setMealsData((prevMeals) => [...prevMeals, ...res.data.meals])
+                    })
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
     }, [setSpecialDishesLoaded, setMealsData])
 
     return (
